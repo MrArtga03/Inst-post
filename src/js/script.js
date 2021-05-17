800ebbc94dae616628b3ec052607437f2b50d925
ilyaTheCreator <respondatanytime@gmail.com>
2021-05-17 07:51:40 +0300

    iluha's commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,19 +2,36 @@ const post = document.querySelector(".post");
 const likesEl = document.querySelector(".likes");
 const heart = document.querySelector(".heart-icon");
 
-likesEl.innerText = "Пока нет ни одного лайка";
+// function for setting likes amount 
+const setLikes = () => likesEl.innerText = `Количество лайков: ${localStorage.getItem("likes")}`
 
-let counter = 0;
-post.addEventListener("dblclick", () => {
-    counter++;
+// localStorage enables you to save some data in the browser 
+// it resets only when a user clears his history with other data like cookies
+// initial check for localStorage info
+if (localStorage.getItem("likes")) {
+    setLikes()
+} else {
+    likesEl.innerText = "Пока нет ни одного лайка";
+}
 
-    if (counter === 1) {
-        likesEl.innerText = counter + " лайк";
+post.addEventListener("dblclick", () => { 
+    // put the counter down in the callback
+    let counter = JSON.parse(localStorage.getItem("likes"));
+
+    // check for localStorage info. If there's so such value, create it 
+    // and assign initial value to 1
+    if (counter) {
+        localStorage.setItem("likes", JSON.stringify(counter + 1))
     } else {
-        likesEl.innerText = counter + " лайка";
+        localStorage.setItem("likes", JSON.stringify(1))
     }
 
+    // removed the if statement
+    // that line went into the function
+    setLikes()
+
     heart.classList.add("animate-like");
+
     setTimeout (() => {
         heart.classList.remove("animate-like");
     }, 800);
